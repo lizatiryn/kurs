5771f6e07b926ef2686ede670d2e8f8315fe5bfe
lizatiryn <116227908+lizatiryn@users.noreply.github.com>
2024-11-12 13:22:43 +0100

    tall oppgave

---
+++ script.js
@@ -0,0 +1,18 @@
+let Num = document.getElementById("number")
+
+let n = Number(Num)
+
+function tallOpp() {
+
+    n++;
+    return document.getElementById("number").innerHTML = ""+n;
+}
+
+function tallNed() {
+
+    n--;
+    return document.getElementById("number").innerHTML = ""+n;
+
+}
+
+console.log(Num)
