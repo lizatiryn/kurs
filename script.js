b6e76ade1c658870fb3a3e470545b074b16ddb30
lizatiryn <116227908+lizatiryn@users.noreply.github.com>
2024-11-14 07:59:23 +0100

    v4

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { set } = require("lodash");
+
 let Num = document.getElementById("number");
 
 let n = Number(Num);
@@ -13,5 +15,79 @@ function tallNed() {
 }
 
 function changeBkgCol() {
-  document.body.style.background = "black";
+  if (document.body.style.backgroundColor === "darkolivegreen") {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("nightMode").innerHTML = "Back to green";
+  } else {
+    document.body.style.backgroundColor = "darkolivegreen";
+    document.getElementById("nightMode").innerHTML = "Night mode";
+  }
+}
+
+function listener(color) {
+  let redCirc = document.getElementById("red");
+  let yellowCirc = document.getElementById("yellow");
+  let greenCirc = document.getElementById("green");
+
+  if (color === "red") {
+    redCirc.classList.remove("trafikk");
+    document.getElementById("red").classList.add("red");
+    yellowCirc.classList.add("trafikk");
+    greenCirc.classList.add("trafikk");
+  } else if (color === "yellow") {
+    redCirc.classList.remove("trafikk");
+    redCirc.classList.add("red");
+    yellowCirc.classList.remove("trafikk");
+    yellowCirc.classList.add("yellow");
+    greenCirc.classList.add("trafikk");
+  } else {
+    redCirc.classList.remove("red");
+    redCirc.classList.add("trafikk");
+    yellowCirc.classList.remove("yellow");
+    yellowCirc.classList.add("trafikk");
+    greenCirc.classList.remove("trafikk");
+    greenCirc.classList.add("green");
+  }
+}
+
+function autoLys() {
+  let redCirc = document.getElementById("red");
+  let yellowCirc = document.getElementById("yellow");
+  let greenCirc = document.getElementById("green");
+
+  setTimeout(() => {
+    redCirc.style.backgroundColor = "aliceblue";
+    yellowCirc.style.backgroundColor = "aliceblue";
+    greenCirc.style.backgroundColor = "aliceblue";
+  }, 100);
+
+  setTimeout(() => {
+    redCirc.style.backgroundColor = "red";
+  }, 3000);
+
+  setTimeout(() => {
+    yellowCirc.style.backgroundColor = "yellow";
+  }, 4000);
+
+  setTimeout(() => {
+    redCirc.style.backgroundColor = "aliceblue";
+    yellowCirc.style.backgroundColor = "aliceblue";
+    greenCirc.style.backgroundColor = "green";
+  }, 5000);
+}
+
+function changeDiv(ver) {
+  let changingDiv = document.getElementById("changing");
+
+  if (ver === "ver1") {
+    changingDiv.classList.remove("ver2");
+    changingDiv.classList.add("ver1");
+    changingDiv.innerHTML =
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt numquam maiores officiis qui ad, iure exercitationem. Tenetur, reiciendis maxime excepturi autem ipsa incidunt cumque adipisci inventore nisi doloribus. Doloribus, laborum.";
+  } else {
+    changingDiv.classList.remove("ver1");
+    changingDiv.classList.add("ver2");
+    changingDiv.innerHTML =
+      "VER2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis eos, omnis sunt perspiciatis est assumenda facilis, molestias exercitationem dolor nam doloremque porro expedita et velit at ipsum quod facere excepturi.";
+  }
 }
